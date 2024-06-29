cfc68cca60402abde78e544f0923988b2f2775a8
viktoriya1234 <vikapav007@hmail.com>
2024-06-29 14:11:13 +0300

    ls22

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/name')
+def index(name=''):
+    return render_template('index.html', name=name)
 
 
 # only local service
@@ -15,9 +16,19 @@ def login():
     return render_template('login.html')
 
 
-@app.route('/vika')
-def vika():
-    return "<h2>My name Vika</h2>"
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html')
+
+
+@app.route('/details')
+def details():
+    return render_template('details.html')
 
 
 if __name__ == '__main__':
